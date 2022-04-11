5ab645146d8bfb2661245d75faeaed03dd358a2e
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2022-04-11 13:02:40 -0300

    Update initpage.js

--- js/initpage.js
+++ js/initpage.js
@@ -1,1 +1,14 @@
-alert("test")
+function showTooltip(n){
+  var ttp = document.getElementById("tooltip")
+
+  if(n == 1)
+  {
+    ttp.style.opacity = "100%";
+    ttp.style.transitionDuration = "2s";
+  }
+  else
+  {
+    ttp.style.opacity = "0%";
+    ttp.style.transitionDuration = "1s";
+  }
+}
