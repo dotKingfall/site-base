09f3dd044d11a4b5b1650a1139b693b8c23cbe95
Gabriel Alves <gabrielhas2001@gmail.com>
2022-04-12 18:02:24 -0300

    Update initpage.js

--- js/initpage.js
+++ js/initpage.js
@@ -13,12 +13,21 @@ function showTooltip(n){
   }
 }
 
+function autoScroll(times){
+  var tmp = setInterval(function (){
+    if(times <= 0) clearInterval(tmp);
+    window.scrollBy(0, 10);
+    times--;
+  }, 10);
+}
+
 function homepage(){
   var hp = document.getElementById("homepage");
-  var pagepos = document.documentElement.getBoundingClientRect();
-  document.getElementById("initpage").style.visibility = "hidden";
-  document.body.style.overflow = "visible";
+  //document.getElementById("initpage").style.visibility = "hidden";
+  //document.body.style.overflow = "visible";
 
   hp.style.position = "static";
   hp.style.visibility = "visible";
+
+  autoScroll(47);
 }
