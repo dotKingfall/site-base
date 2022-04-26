9bd1251fe5be5fdbc5f2a620b95b84ad74cd6a2b
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2022-04-26 20:47:55 -0300

    Update auth.js

--- js/auth/auth.js
+++ js/auth/auth.js
@@ -1,3 +1,6 @@
-function verify(username, password){
-  //todo
+function verify(){
+  username = document.getElementById("uid");
+  password = document.getElementById("uid_pw");
+
+  alert(`Usuário: ${username.value}\nSenha: ${password.value}`);
 }
