2f25d089a4ae10b5176e13593cfbd153275d84f2
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2022-04-30 17:35:02 -0300

    Update auth.js

--- js/auth/auth.js
+++ js/auth/auth.js
@@ -10,13 +10,13 @@ function pwvisibility(element){
   if(pwvis == false){
     document.getElementById("uid_pw").type = "text";
     element.src = "../img/auth/closed-eye.svg";
-    element.style.transform = "scale(1.25) translateY(88.5%)";
+    element.style.transform = "scale(1.25) translateY(79%)";
     pwvis = true;
   }
   else{
     document.getElementById("uid_pw").type = "password";
     element.src = "../img/auth/open-eye.svg";
-    element.style.transform = "scale(1.1) translateY(100%)";
+    element.style.transform = "scale(1.1) translateY(90%)";
     pwvis = false;
   }
 }
