41b5bfab0617327ae634f4862897cf9c53599993
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2022-05-04 10:42:03 -0300

    Update player.js

--- js/index/player.js
+++ js/index/player.js
@@ -1,1 +1,12 @@
-//
+var player_active = false;
+function showPlayer(){
+  ple = document.getElementById("player");
+  if(player_active == false){
+    ple.style.bottom = "0%";
+    player_active = true;
+  }
+  else{
+    ple.style.bottom = "-15%";
+    player_active = false;
+  }
+}
