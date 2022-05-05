5ef76435ff04f3fa7bb2d00b7e0d428ececece79
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2022-05-05 19:27:55 -0300

    Create player.js

---
+++ js/player.js
@@ -0,0 +1,17 @@
+var musicPlaying = false; //Se alguma música estiver tocando, essa variável deve ser true
+function playsong(){
+  //
+}
+
+var player_active = false;
+function showPlayer(){
+  ple = document.getElementById("playerws");
+  if(player_active == false){
+    ple.style.height = "90px";
+    player_active = true;
+  }
+  else{
+    ple.style.height = "0px";
+    player_active = false;
+  }
+}
