be9a78f89fa0c882259caa3549065452c2f846a5
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2022-05-06 12:37:59 -0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,17 +1,20 @@
 var musicPlaying = false; //Se alguma música estiver tocando, essa variável deve ser true
-function playsong(){
-  //
+function playsong(c, n = 0){
+  if(c == 1){}
+  else if(c == 2){}
+
+  if(n == 1){}
 }
 
 var player_active = false;
 function showPlayer(){
   ple = document.getElementById("playerws");
   if(player_active == false){
-    ple.style.height = "90px";
+    ple.style.width = "180px";
     player_active = true;
   }
   else{
-    ple.style.height = "0px";
+    ple.style.width = "0px";
     player_active = false;
   }
 }
