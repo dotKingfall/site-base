86ad61631bf1fa85b81eb04793808191e9a05a53
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2022-05-07 19:04:01 -0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -23,20 +23,7 @@ function shouldOverflow(){
 }
 
 function copysongname(){
-  var copymessage = document.getElementById("copybutton").children[1];
-  await navigator.clipboard.writeText(`${songname} - ${singer}`).then(() => {
-    if(copymessage.innerText === "COPIAR"){
-      copymessage.innerText = "COPIADO!";
-      setTimeout(function (){copymessage.innerText = "COPIAR"}, 2000);
-    }
-  })
-  .catch(() => {
-    if(copymessage.innerText === "COPIAR"){
-      copymessage.innerText = "NÃO COPIADO!";
-      setTimeout(function (){copymessage.innerText = "COPIAR"}, 2000);
-    }
-  })
-  ;
+  navigator.clipboard.writeText("aaaa");
 }
 
 var player_active = false;
